86020187e3ef09330db4d00d3b35eff73a097734
Aditya Ralhan <adityaralhan2005@gmail.com>
2024-08-08 21:44:21 +0530

    hi

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ['framer-motion'],
+    },
+  },
+  
   plugins: [react()],
   css: {
     modules: {
