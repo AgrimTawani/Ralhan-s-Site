1f3a9ddb2ffa1a287f665a4f37ae7f843f9fdb73
Agrim <agrimtawani139@gmail.com>
2024-08-08 22:25:04 +0530

    efsd

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './Intro.module.css'
-import { motion } from './dist/framer-motion';
+import { motion } from 'framer-motion';
 
 const textVariants = {
     initial: {
